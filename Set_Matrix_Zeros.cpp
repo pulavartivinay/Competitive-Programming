3538afa8ea5154efdf41868bed369fb76aabd329
Pulavarti vinay kumar <56190203+111801034@users.noreply.github.com>
2020-09-16 16:56:13 +0530

    comments added

--- Set_Matrix_Zeros.cpp
+++ Set_Matrix_Zeros.cpp
@@ -1,41 +1,41 @@
-/* Given a matrix, A of size M x N of 0s and 1s. If an element is 0, set its entire row and column to 0.
+// Given a matrix, A of size M x N of 0s and 1s. If an element is 0, set its entire row and column to 0.
 
-Note: This will be evaluated on the extra memory used. Try to minimize the space and time complexity.
+// Note: This will be evaluated on the extra memory used. Try to minimize the space and time complexity.
 
 
 
-Input Format:
+// Input Format:
 
-The first and the only argument of input contains a 2-d integer matrix, A, of size M x N.
-Output Format:
+// The first and the only argument of input contains a 2-d integer matrix, A, of size M x N.
+// Output Format:
 
-Return a 2-d matrix that satisfies the given conditions.
-Constraints:
+// Return a 2-d matrix that satisfies the given conditions.
+// Constraints:
 
-1 <= N, M <= 1000
-0 <= A[i][j] <= 1
-Examples:
+// 1 <= N, M <= 1000
+// 0 <= A[i][j] <= 1
+// Examples:
 
-Input 1:
-    [   [1, 0, 1],
-        [1, 1, 1], 
-        [1, 1, 1]   ]
+// Input 1:
+//     [   [1, 0, 1],
+//         [1, 1, 1], 
+//         [1, 1, 1]   ]
 
-Output 1:
-    [   [0, 0, 0],
-        [1, 0, 1],
-        [1, 0, 1]   ]
+// Output 1:
+//     [   [0, 0, 0],
+//         [1, 0, 1],
+//         [1, 0, 1]   ]
 
-Input 2:
-    [   [1, 0, 1],
-        [1, 1, 1],
-        [1, 0, 1]   ]
+// Input 2:
+//     [   [1, 0, 1],
+//         [1, 1, 1],
+//         [1, 0, 1]   ]
 
-Output 2:
-    [   [0, 0, 0],
-        [1, 0, 1],
-        [0, 0, 0]   ]
-   */
+// Output 2:
+//     [   [0, 0, 0],
+//         [1, 0, 1],
+//         [0, 0, 0]   ]
+  
  
 Solution:
   void Solution::setZeroes(vector<vector<int> > &A) {
