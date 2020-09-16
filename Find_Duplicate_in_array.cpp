113863cac4028797ed2055415a8d4e8d0a7c65da
Pulavarti vinay kumar <56190203+111801034@users.noreply.github.com>
2020-09-16 16:58:38 +0530

    Update Find_Duplicate_in_array.cpp

--- Find_Duplicate_in_array.cpp
+++ Find_Duplicate_in_array.cpp
@@ -1,14 +1,14 @@
-/* Given a read only array of n + 1 integers between 1 and n, find one number that repeats in linear time using less than O(n) space and traversing the stream sequentially O(1) times.
+// Given a read only array of n + 1 integers between 1 and n, find one number that repeats in linear time using less than O(n) space and traversing the stream sequentially O(1) times.
 
-Sample Input:
+// Sample Input:
 
-[3 4 1 4 1]
-Sample Output:
+// [3 4 1 4 1]
+// Sample Output:
 
-1
-If there are multiple possible answers ( like in the sample case above ), output any one.
+// 1
+// If there are multiple possible answers ( like in the sample case above ), output any one.
 
-If there is no duplicate, output -1 .*/
+// If there is no duplicate, output -1 .
 
 Solution: 
   int Solution::repeatedNumber(const vector<int> &A) {
