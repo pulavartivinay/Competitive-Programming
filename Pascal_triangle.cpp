74815b49f69d9564433fae92b11f95300b387328
Pulavarti vinay kumar <56190203+111801034@users.noreply.github.com>
2020-09-16 20:17:30 +0530

    Update Pascal_triangle.cpp

--- Pascal_triangle.cpp
+++ Pascal_triangle.cpp
@@ -1,20 +1,20 @@
-Given numRows, generate the first numRows of Pascal’s triangle.
+// Given numRows, generate the first numRows of Pascal’s triangle.
 
-Pascal’s triangle : To generate A[C] in row R, sum up A’[C] and A’[C-1] from previous row R - 1.
+// Pascal’s triangle : To generate A[C] in row R, sum up A’[C] and A’[C-1] from previous row R - 1.
 
-Example:
+// Example:
 
-Given numRows = 5,
+// Given numRows = 5,
 
-Return
+// Return
 
-[
-     [1],
-     [1,1],
-     [1,2,1],
-     [1,3,3,1],
-     [1,4,6,4,1]
-]
+// [
+//      [1],
+//      [1,1],
+//      [1,2,1],
+//      [1,3,3,1],
+//      [1,4,6,4,1]
+// ]
 
 Solution:
   vector<vector<int> > Solution::solve(int A) {
