ef114cf950495a03c11ed047514b4e0a14b6bd52
Pulavarti vinay kumar <56190203+111801034@users.noreply.github.com>
2020-09-16 19:40:24 +0530

    Update Wave_Array.cpp

--- Wave_Array.cpp
+++ Wave_Array.cpp
@@ -1,14 +1,14 @@
-Given an array of integers, sort the array into a wave like array and return it,
-In other words, arrange the elements into a sequence such that a1 >= a2 <= a3 >= a4 <= a5.....
+// Given an array of integers, sort the array into a wave like array and return it,
+// In other words, arrange the elements into a sequence such that a1 >= a2 <= a3 >= a4 <= a5.....
 
-Example
+// Example
 
-Given [1, 2, 3, 4]
+// Given [1, 2, 3, 4]
 
-One possible answer : [2, 1, 4, 3]
-Another possible answer : [4, 1, 3, 2]
- NOTE : If there are multiple answers possible, return the one thats lexicographically smallest.
-So, in example case, you will return [2, 1, 4, 3]
+// One possible answer : [2, 1, 4, 3]
+// Another possible answer : [4, 1, 3, 2]
+//  NOTE : If there are multiple answers possible, return the one thats lexicographically smallest.
+// So, in example case, you will return [2, 1, 4, 3]
 
 Solution:
   vector<int> Solution::wave(vector<int> &A) {
