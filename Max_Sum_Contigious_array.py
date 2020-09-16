a214cb77928a1b5d7bd02b0558772832af5ee549
Pulavarti vinay kumar <56190203+111801034@users.noreply.github.com>
2020-09-16 16:59:07 +0530

    Update Max_Sum_Contigious_array.py

--- Max_Sum_Contigious_array.py
+++ Max_Sum_Contigious_array.py
@@ -1,25 +1,25 @@
-/*Find the contiguous subarray within an array, A of length N which has the largest sum.
+# /*Find the contiguous subarray within an array, A of length N which has the largest sum.
 
-Input Format:
+# Input Format:
 
-The first and the only argument contains an integer array, A.
-Output Format:
+# The first and the only argument contains an integer array, A.
+# Output Format:
 
-Return an integer representing the maximum possible sum of the contiguous subarray.
-Constraints:
+# Return an integer representing the maximum possible sum of the contiguous subarray.
+# Constraints:
 
-1 <= N <= 1e6
--1000 <= A[i] <= 1000
-For example:
+# 1 <= N <= 1e6
+# -1000 <= A[i] <= 1000
+# For example:
 
-Input 1:
-    A = [1, 2, 3, 4, -10]
+# Input 1:
+#     A = [1, 2, 3, 4, -10]
 
-Output 1:
-    10
+# Output 1:
+#     10
 
-Explanation 1:
-    The subarray [1, 2, 3, 4] has the maximum possible sum of 10. */
+# Explanation 1:
+#     The subarray [1, 2, 3, 4] has the maximum possible sum of 10. */
     
  Solution:
     def maxSubArray(self, A):
