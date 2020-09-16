16b4626c7575ae8ef4bc419f555facbe5d160bbf
Pulavarti vinay kumar <56190203+111801034@users.noreply.github.com>
2020-09-16 23:23:01 +0530

    Update and rename Spatial_order_matrix.cpp to Spiral_order_matrix.cpp

--- Spiral_order_matrix.cpp
+++ Spiral_order_matrix.cpp
@@ -1,24 +1,24 @@
-Given an integer A, generate a square matrix filled with elements from 1 to A2 in spiral order
-Input Format:
-The first and the only argument contains an integer, A.
-Output Format:
-Return a 2-d matrix of size A x A satisfying the spiral order.
-Constraints:
-1 <= A <= 1000
-Examples:
-Input 1:
-    A = 3
-Output 1:
-    [   [ 1, 2, 3 ],
-        [ 8, 9, 4 ],
-        [ 7, 6, 5 ]   ]
-Input 2:
-    4
-Output 2:
-    [   [1, 2, 3, 4],
-        [12, 13, 14, 5],
-        [11, 16, 15, 6],
-        [10, 9, 8, 7]   ]
+// Given an integer A, generate a square matrix filled with elements from 1 to A2 in spiral order
+// Input Format:
+// The first and the only argument contains an integer, A.
+// Output Format:
+// Return a 2-d matrix of size A x A satisfying the spiral order.
+// Constraints:
+// 1 <= A <= 1000
+// Examples:
+// Input 1:
+//     A = 3
+// Output 1:
+//     [   [ 1, 2, 3 ],
+//         [ 8, 9, 4 ],
+//         [ 7, 6, 5 ]   ]
+// Input 2:
+//     4
+// Output 2:
+//     [   [1, 2, 3, 4],
+//         [12, 13, 14, 5],
+//         [11, 16, 15, 6],
+//         [10, 9, 8, 7]   ]
        
 Solution:
   vector<vector<int> > Solution::generateMatrix(int A) {
