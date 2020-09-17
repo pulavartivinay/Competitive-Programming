167d6ee4921b6809fa65d2de8307a5dce60149b5
Pulavarti vinay kumar <56190203+111801034@users.noreply.github.com>
2020-09-17 18:33:44 +0530

    Update Power_of_Two_integers.cpp

--- Power_of_Two_integers.cpp
+++ Power_of_Two_integers.cpp
@@ -1,10 +1,10 @@
-Given a positive integer which fits in a 32 bit signed integer, find if it can be expressed as A^P where P > 1 and A > 0. A and P both should be integers.
+// Given a positive integer which fits in a 32 bit signed integer, find if it can be expressed as A^P where P > 1 and A > 0. A and P both should be integers.
 
-Example
+// Example
 
-Input : 4
-Output : True  
-as 2^2 = 4. 
+// Input : 4
+// Output : True  
+// as 2^2 = 4. 
 
 Solution:
   int Solution::isPower(int A) {
