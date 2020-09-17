1364ba5bd76b6360e8a957bab7bf2d2fd5caf56e
Pulavarti vinay kumar <56190203+111801034@users.noreply.github.com>
2020-09-17 23:17:38 +0530

    Update Excel_Column_Number.cpp

--- Excel_Column_Number.cpp
+++ Excel_Column_Number.cpp
@@ -1,11 +1,11 @@
-Given a column title A as appears in an Excel sheet, return its corresponding column number.
-Problem Constraints
-1 <= |A| <= 100
-Input Format
-First and only argument is string A.
-Output Format
-Return an integer
-Example : AB --> 28;
+// Given a column title A as appears in an Excel sheet, return its corresponding column number.
+// Problem Constraints
+// 1 <= |A| <= 100
+// Input Format
+// First and only argument is string A.
+// Output Format
+// Return an integer
+// Example : AB --> 28;
 
 Solution:
   int Solution::titleToNumber(string A) {
