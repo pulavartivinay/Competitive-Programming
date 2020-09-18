da5bde0d99f6dacaaff17a5cbeef4f3da46e8f7b
Pulavarti vinay kumar <56190203+111801034@users.noreply.github.com>
2020-09-18 19:50:04 +0530

    Update Palindrome_integer.cpp

--- Palindrome_integer.cpp
+++ Palindrome_integer.cpp
@@ -1,15 +1,15 @@
-Determine whether an integer is a palindrome. Do this without extra space.
+// Determine whether an integer is a palindrome. Do this without extra space.
 
-A palindrome integer is an integer x for which reverse(x) = x where reverse(x) is x with its digit reversed.
-Negative numbers are not palindromic.
+// A palindrome integer is an integer x for which reverse(x) = x where reverse(x) is x with its digit reversed.
+// Negative numbers are not palindromic.
 
-Example :
+// Example :
 
-Input : 12121
-Output : True
+// Input : 12121
+// Output : True
 
-Input : 123
-Output : False
+// Input : 123
+// Output : False
 
 Solution:
   int Solution::isPalindrome(int A) {
