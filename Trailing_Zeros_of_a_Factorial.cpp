a6be92abfb4ec241150cb67452c7d8294f8f8e98
Pulavarti vinay kumar <56190203+111801034@users.noreply.github.com>
2020-09-20 18:27:06 +0530

    added hints

--- Trailing_Zeros_of_a_Factorial.cpp
+++ Trailing_Zeros_of_a_Factorial.cpp
@@ -1,26 +1,26 @@
-Given an integer A, return the number of trailing zeroes in A!.
-Note: Your solution should be in logarithmic time complexity.
-**Problem Constraints**
-1 <= A <= 10000
-**Input Format**
-First and only argumment is integer A.
-**Output Format**
-Return an integer, the answer to the problem.
-**Example Input**
-Input 1:
- A = 4
-Input 2:
- A = 5
-**Example Output**
-Output 1:
- 0
-Output 2:
- 1
-**Example Explanation**
-Explanation 1:
- 4! = 24
-Explanation 2:
- 5! = 120
+// Given an integer A, return the number of trailing zeroes in A!.
+// Note: Your solution should be in logarithmic time complexity.
+// **Problem Constraints**
+// 1 <= A <= 10000
+// **Input Format**
+// First and only argumment is integer A.
+// **Output Format**
+// Return an integer, the answer to the problem.
+// **Example Input**
+// Input 1:
+//  A = 4
+// Input 2:
+//  A = 5
+// **Example Output**
+// Output 1:
+//  0
+// Output 2:
+//  1
+// **Example Explanation**
+// Explanation 1:
+//  4! = 24
+// Explanation 2:
+//  5! = 120
  
  Solution:
   int Solution::trailingZeroes(int A) {
