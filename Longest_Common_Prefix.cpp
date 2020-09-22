ef375c248f50c4211b6e6cb79aaa8ee25172b1bd
Pulavarti vinay kumar <56190203+111801034@users.noreply.github.com>
2020-09-22 15:57:26 +0530

    Update Longest_Common_Prefix.cpp

--- Longest_Common_Prefix.cpp
+++ Longest_Common_Prefix.cpp
@@ -1,25 +1,25 @@
-Given the array of strings A,
-you need to find the longest string S which is the prefix of ALL the strings in the array.
-Longest common prefix for a pair of strings S1 and S2 is the longest string S which is the prefix of both S1
-and S2.
-For Example, longest common prefix of "abcdefgh" and "abcefgh" is "abc".
-Input Format
-The only argument given is an array of strings A.
-Output Format
-Return longest common prefix of all strings in A.
-For Example
-Input 1:
-    A = ["abcdefgh", "aefghijk", "abcefgh"]
-Output 1:
-    "a"
-    Explanation 1:
-        Longest common prefix of all the strings is "a".
-Input 2:
-    A = ["abab", "ab", "abcd"];
-Output 2:
-    "ab"
-    Explanation 2:
-        Longest common prefix of all the strings is "ab".
+// Given the array of strings A,
+// you need to find the longest string S which is the prefix of ALL the strings in the array.
+// Longest common prefix for a pair of strings S1 and S2 is the longest string S which is the prefix of both S1
+// and S2.
+// For Example, longest common prefix of "abcdefgh" and "abcefgh" is "abc".
+// Input Format
+// The only argument given is an array of strings A.
+// Output Format
+// Return longest common prefix of all strings in A.
+// For Example
+// Input 1:
+//     A = ["abcdefgh", "aefghijk", "abcefgh"]
+// Output 1:
+//     "a"
+//     Explanation 1:
+//         Longest common prefix of all the strings is "a".
+// Input 2:
+//     A = ["abab", "ab", "abcd"];
+// Output 2:
+//     "ab"
+//     Explanation 2:
+//         Longest common prefix of all the strings is "ab".
         
 Solution:
   string commonPrefixUtil(string str1, string str2) 
