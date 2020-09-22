e2ebab3fb893142bfa57f762f65102b71048b6c1
Pulavarti vinay kumar <56190203+111801034@users.noreply.github.com>
2020-09-22 16:48:33 +0530

    Update Roman_To_Int.cpp

--- Roman_To_Int.cpp
+++ Roman_To_Int.cpp
@@ -1,21 +1,21 @@
-Given a string A representing a roman numeral.
-Convert A into integer.
-A is guaranteed to be within the range from 1 to 3999.
-NOTE: Read more
-details about roman numerals at Roman Numeric System
-Input Format
-The only argument given is string A.
-Output Format
-Return an integer which is the integer verison of roman numeral string.
-For Example
-Input 1:
-    A = "XIV"
-Output 1:
-    14
-Input 2:
-    A = "XX"
-Output 2:
-    20
+// Given a string A representing a roman numeral.
+// Convert A into integer.
+// A is guaranteed to be within the range from 1 to 3999.
+// NOTE: Read more
+// details about roman numerals at Roman Numeric System
+// Input Format
+// The only argument given is string A.
+// Output Format
+// Return an integer which is the integer verison of roman numeral string.
+// For Example
+// Input 1:
+//     A = "XIV"
+// Output 1:
+//     14
+// Input 2:
+//     A = "XX"
+// Output 2:
+//     20
     
 Solution:
   int Solution::romanToInt(string A) {
