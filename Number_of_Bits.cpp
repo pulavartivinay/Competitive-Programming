cdc92db69417607beb03f34fd9b7dc65bd314254
Pulavarti vinay kumar <56190203+111801034@users.noreply.github.com>
2020-09-25 16:11:48 +0530

    Update Number_of_Bits.cpp

--- Number_of_Bits.cpp
+++ Number_of_Bits.cpp
@@ -1,8 +1,9 @@
-Write a function that takes an unsigned integer and returns the number of 1 bits it has.
-Example:
-The 32-bit integer 11 has binary representation
-00000000000000000000000000001011
-so the function should return 3.
+// Write a function that takes an unsigned integer and returns the number of 1 bits it has.
+// Example:
+// The 32-bit integer 11 has binary representation
+// 00000000000000000000000000001011
+// so the function should return 3.
+
 Solution:
   #include <bits/stdc++.h> 
 int Solution::numSetBits(unsigned int A) {
