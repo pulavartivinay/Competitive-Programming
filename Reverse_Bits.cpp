3af50471be9ec46e3a2b1f7ec5dd9df33ae45dba
Pulavarti vinay kumar <56190203+111801034@users.noreply.github.com>
2020-09-25 16:13:26 +0530

    Create Reverse_Bits.cpp

---
+++ Reverse_Bits.cpp
@@ -0,0 +1,40 @@
+Reverse the bits of an 32 bit unsigned integer A.
+Problem Constraints
+0 <= A <= 2**32
+Input Format
+First and only argument of input contains an integer A.
+Output Format
+Return a single unsigned integer denoting the decimal value of reversed bits.
+Example Input
+Input 1:
+ 0
+Input 2:
+ 3
+Example Output
+Output 1:
+ 0
+Output 2:
+ 3221225472
+Example Explanation
+Explanation 1:
+        00000000000000000000000000000000
+=>      00000000000000000000000000000000
+Explanation 2:
+        00000000000000000000000000000011    
+=>      11000000000000000000000000000000
+
+Solution:
+  #include<bits/stdc++.h>
+unsigned int Solution::reverse(unsigned int A) {
+    string s = bitset<32> (A).to_string();
+    unsigned int li = 0;
+    int n = s.size();
+    for(int i=0;i<n;i++)
+    {
+        if(s[i] == '1')
+        {
+            li = li + pow(2,i);
+        }
+    }
+    return li;
+}
