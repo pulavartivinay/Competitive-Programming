32d0a5753d691316782b45fedbd66551e35326ca
Pulavarti vinay kumar <56190203+111801034@users.noreply.github.com>
2020-09-25 20:19:13 +0530

    Update Rearrange_Array.cpp

--- Rearrange_Array.cpp
+++ Rearrange_Array.cpp
@@ -1,10 +1,10 @@
-Rearrange a given array so that Arr[i] becomes Arr[Arr[i]] with O(1) extra space.
-Example:
-Input : [1, 0]
-Return : [0, 1]
- Lets say N = size of the array. Then, following holds true :
-1. All elements in the array are in the range [0, N-1].
-2. N * N does not overflow for a signed integer.
+// Rearrange a given array so that Arr[i] becomes Arr[Arr[i]] with O(1) extra space.
+// Example:
+// Input : [1, 0]
+// Return : [0, 1]
+//  Lets say N = size of the array. Then, following holds true :
+// 1. All elements in the array are in the range [0, N-1].
+// 2. N * N does not overflow for a signed integer.
 
 Solution:
   void Solution::arrange(vector<int> &A) {
