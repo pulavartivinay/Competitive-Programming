4912e77f9b0103970a2b855f16b1a0c4a5dd3db9
Pulavarti vinay kumar <56190203+111801034@users.noreply.github.com>
2020-10-05 17:22:24 +0530

    Update Minimum_XOR.cpp

--- Minimum_XOR.cpp
+++ Minimum_XOR.cpp
@@ -1,22 +1,22 @@
-Given an integer array A of N integers, find the pair of integers in the array which have minimum XOR value. Report the minimum XOR value.
-Input Format:
-    First and only argument of input contains an integer array A
-Output Format:
-    return a single integer denoting minimum xor value
-Constraints:
-2 <= N <= 100 000  
-0 <= A[i] <= 1 000 000 000
-For Examples :
-Example Input 1:
-    A = [0, 2, 5, 7]
-Example Output 1:
-    2
-Explanation:
-    0 xor 2 = 2
-Example Input 2:
-    A = [0, 4, 7, 9]
-Example Output 2:
-    3
+// Given an integer array A of N integers, find the pair of integers in the array which have minimum XOR value. Report the minimum XOR value.
+// Input Format:
+//     First and only argument of input contains an integer array A
+// Output Format:
+//     return a single integer denoting minimum xor value
+// Constraints:
+// 2 <= N <= 100 000  
+// 0 <= A[i] <= 1 000 000 000
+// For Examples :
+// Example Input 1:
+//     A = [0, 2, 5, 7]
+// Example Output 1:
+//     2
+// Explanation:
+//     0 xor 2 = 2
+// Example Input 2:
+//     A = [0, 4, 7, 9]
+// Example Output 2:
+//     3
 
 Solution:
   int Solution::findMinXor(vector<int> &A) {
