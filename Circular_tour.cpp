6dd0271ecff6b49c99b33882a5bff3e595605a34
Pulavarti vinay kumar <pulavartivinay@gmail.com>
2021-06-16 21:35:33 +0530

    Update Circular_tour.cpp

--- Circular_tour.cpp
+++ Circular_tour.cpp
@@ -2,31 +2,31 @@ Question Link: https://practice.geeksforgeeks.org/problems/circular-tour/1#
 
 Solution :
 
-    int tour(petrolPump p[],int n)
-    {
-       //Your code here
-       int ans = -1;
-       for(int i=0;i<n;i++)
+int tour(petrolPump p[],int n)
+{
+   //Your code here
+   int ans = -1;
+   for(int i=0;i<n;i++)
+   {
+       if(p[i].petrol > p[i].distance)
        {
-           if(p[i].petrol > p[i].distance)
+           int total_petrol = 0;
+           int total_distance = 0;
+           for(int j=i;j<(i+n);j++)
            {
-               int total_petrol = 0;
-               int total_distance = 0;
-               for(int j=i;j<(i+n);j++)
+               total_petrol = total_petrol + p[j%n].petrol;
+               total_distance = total_distance + p[j%n].distance;
+               if(total_petrol < total_distance)
                {
-                   total_petrol = total_petrol + p[j%n].petrol;
-                   total_distance = total_distance + p[j%n].distance;
-                   if(total_petrol < total_distance)
-                   {
-                       break;
-                   }
-               }
-               if(total_petrol >= total_distance)
-               {
-                   ans = i;
                    break;
                }
            }
+           if(total_petrol >= total_distance)
+           {
+               ans = i;
+               break;
+           }
        }
-       return ans;
-    }
+   }
+   return ans;
+}
