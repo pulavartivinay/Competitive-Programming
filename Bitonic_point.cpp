f29d43966d6ced4ffaf84a753bda20cdd6027238
Pulavarti vinay kumar <pulavartivinay@gmail.com>
2021-07-07 14:53:38 +0530

    Done optimally of TC- Log(n) and SC- O(1)

--- Bitonic_point.cpp
+++ Bitonic_point.cpp
@@ -1,9 +1,27 @@
 Question Link: https://practice.geeksforgeeks.org/problems/maximum-value-in-a-bitonic-array3001/1
 
 
-Solution Link:
-	int findMaximum(int arr[], int n) {
-	    // code here
-	    sort(arr, arr+n);
-	    return arr[n-1];
+Solution:
+
+int findMaximum(int arr[], int n) {
+    // code here
+    int low = 0;
+    int high = n-1;
+    while(low <= high)
+    {
+	if(low == high)
+	{
+	    return arr[low];
 	}
+	int mid = (low+high)/2;
+	if(arr[mid] < arr[mid+1])
+	{
+	    low = mid + 1;
+	}
+	else if(arr[mid] > arr[mid+1])
+	{
+	    high = mid;
+	}
+    }
+}
+
