cc3fb70e856fa7f1ecf33177e04084a791faef06
Pulavarti vinay kumar <pulavartivinay@gmail.com>
2021-07-15 13:51:01 +0530

    Update Reverse_Bits.cpp

--- Reverse_Bits.cpp
+++ Reverse_Bits.cpp
@@ -1,40 +1,13 @@
-// Reverse the bits of an 32 bit unsigned integer A.
-// Problem Constraints
-// 0 <= A <= 2**32
-// Input Format
-// First and only argument of input contains an integer A.
-// Output Format
-// Return a single unsigned integer denoting the decimal value of reversed bits.
-// Example Input
-// Input 1:
-//  0
-// Input 2:
-//  3
-// Example Output
-// Output 1:
-//  0
-// Output 2:
-//  3221225472
-// Example Explanation
-// Explanation 1:
-//         00000000000000000000000000000000
-// =>      00000000000000000000000000000000
-// Explanation 2:
-//         00000000000000000000000000000011    
-// =>      11000000000000000000000000000000
+Question Link: https://practice.geeksforgeeks.org/problems/reverse-bits3556/1
 
 Solution:
-  #include<bits/stdc++.h>
-unsigned int Solution::reverse(unsigned int A) {
-    string s = bitset<32> (A).to_string();
-    unsigned int li = 0;
-    int n = s.size();
-    for(int i=0;i<n;i++)
+
+long long reversedBits(long long X) {
+    // code here
+    long long int ans = 0;
+    for(int i=0;i<32;i++)
     {
-        if(s[i] == '1')
-        {
-            li = li + pow(2,i);
-        }
+        if(X & (1 << i)) ans += pow(2,31-i);
     }
-    return li;
+    return ans;
 }
