bcc0dff64e083ca45d761d3b5f01fbfcaad9f1f1
Pulavarti vinay kumar <pulavartivinay@gmail.com>
2021-08-04 20:47:41 +0530

    updated the solution with less space complexity

--- Sequence_of_Sequence.cpp
+++ Sequence_of_Sequence.cpp
@@ -2,29 +2,30 @@ Question Link: https://practice.geeksforgeeks.org/problems/sequence-of-sequence1
 
 Solution:
 
-void DFS(int i,vector<int>& temp,int& count,vector<int>& vis,int n,int m)
+void DFS(int i,int& temp,int& count,vector<int>& vis,int n,int m)
 {
     if(i<=0 or i>m) return;
     if(vis[i]) return;
-    if(temp.size() > n) return;
+    if(temp > n) return;
 
     vis[i] = 1;
-    temp.push_back(i);
-    if(temp.size() == n) count++;
+    temp++;
+    if(temp == n) count++;
     for(int j=2*i;j<=m;j++)
     {
         DFS(j,temp,count,vis,n,m);
     }
-    temp.pop_back();
+    temp--;
     vis[i] = 0;
 }
-int numberSequence(int m, int n){
+int numberSequence(int m, int n)
+{
     // code here
     int ans = 0;
     vector<int> vis(m+1,0);
     for(int i=1;i<=m;i++)
     {
-        vector<int> temp;
+        int temp = 0;
         int count = 0;
         DFS(i,temp,count,vis,n,m);
         ans += count;
