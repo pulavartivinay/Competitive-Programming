3c28e3e6c95c703e68dec2b04e0c4f8f9769476b
Pulavarti vinay kumar <pulavartivinay@gmail.com>
2021-10-13 12:21:53 +0530

    Create Interleaved_Strings.cpp

---
+++ Dynamic Programming/Interleaved_Strings.cpp
@@ -0,0 +1,27 @@
+Question Link: https://practice.geeksforgeeks.org/problems/interleaved-strings/1
+
+Solution:
+
+bool isInterleave(string A, string B, string C) 
+{
+    //Your code here
+    int a = A.length();
+    int b = B.length();
+    int c = C.length();
+    if(a+b != c) return false;
+    bool dp[a+1][b+1];
+    memset(dp, 0, sizeof(dp));
+    for(int i=0;i<=a;i++)
+    {
+        for(int j=0;j<=b;j++)
+        {
+            if(i == 0 && j == 0) dp[i][j] = 1;
+            else if(i == 0 && B[j-1] == C[j-1]) dp[i][j] = dp[i][j-1];
+            else if(j == 0 && A[i-1] == C[i-1]) dp[i][j] = dp[i-1][j];
+            else if(A[i-1] == C[i+j-1] && B[j-1] != C[i+j-1]) dp[i][j] = dp[i-1][j];
+            else if(A[i-1] != C[i+j-1] && B[j-1] == C[i+j-1]) dp[i][j] = dp[i][j-1];
+            else if(A[i-1] == C[i+j-1] && B[j-1] == C[i+j-1]) dp[i][j] = (dp[i-1][j] | dp[i][j-1]);
+        }
+    }
+    return dp[a][b];
+}
