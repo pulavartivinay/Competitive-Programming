3eb820035569438f3735199cdf02965bc1079f12
Pulavarti vinay kumar <pulavartivinay@gmail.com>
2022-04-21 23:32:03 +0530

    Update Spiral_order_matrix.cpp

--- Spiral_order_matrix.cpp
+++ Spiral_order_matrix.cpp
@@ -1,65 +1,44 @@
-// Given an integer A, generate a square matrix filled with elements from 1 to A2 in spiral order
-// Input Format:
-// The first and the only argument contains an integer, A.
-// Output Format:
-// Return a 2-d matrix of size A x A satisfying the spiral order.
-// Constraints:
-// 1 <= A <= 1000
-// Examples:
-// Input 1:
-//     A = 3
-// Output 1:
-//     [   [ 1, 2, 3 ],
-//         [ 8, 9, 4 ],
-//         [ 7, 6, 5 ]   ]
-// Input 2:
-//     4
-// Output 2:
-//     [   [1, 2, 3, 4],
-//         [12, 13, 14, 5],
-//         [11, 16, 15, 6],
-//         [10, 9, 8, 7]   ]
-       
+Question Link: https://leetcode.com/problems/spiral-matrix/
+
 Solution:
-  vector<vector<int> > Solution::generateMatrix(int A) {
-    vector<vector<int> > b( A , vector<int> (A, 0));
-    int count=0;
-    int value = 1;
-    int m = A;
-    int n = A;
-    int k,l = 0;
-    while(k < m and l < n)
-    {
-        for(int i=l;i<n;i++)
-        {
-            b[k][i] = value;
-            value = value + 1;
-        }
-        k = k + 1;
-        for(int i=k;i<m;i++)
-        {
-            b[i][n-1] = value;
-            value = value + 1;
-        }
-        n = n - 1;
-        if(k < m)
-        {
-            for(int i=n-1;i>=l;i--)
-            {
-                b[m-1][i] = value;
-                value = value + 1;
-            }
-        }
-        m = m - 1;
-        if(l < n)
-        {
-            for(int i=m-1;i>=k;i--)
-            {
-                b[i][l] = value;
-                value = value+1;
-            }
-        }
-        l = l + 1;
-    }
-    return b;
+
+vector<int> spiralOrder(vector<vector<int>>& matrix) 
+{
+ vector<int> ans;
+ int m = matrix.size();
+ int n = matrix[0].size();
+ int top = 0, right = n-1, bottom = m-1, left = 0;
+ while(top <= bottom && left <= right)
+ {
+     // first row of matrix
+     for(int i=left;i<=right;i++)
+     {
+         ans.push_back(matrix[top][i]);
+     }
+     top++;
+     if(top > bottom) break;
+
+     // last col of matrix
+     for(int i=top;i<=bottom;i++)
+     {
+         ans.push_back(matrix[i][right]);
+     }
+     right--;
+     if(left > right) break;
+
+     // last row of matrix
+     for(int i=right;i>=left;i--)
+     {
+         ans.push_back(matrix[bottom][i]);
+     }
+     bottom--;
+
+     // first col of matrix
+     for(int i=bottom;i>=top;i--)
+     {
+         ans.push_back(matrix[i][left]);
+     }
+     left++;
+ }
+ return ans;
 }
