13a43040da51329f1aecdb50d2e300a6382467ee
Pulavarti vinay kumar <pulavartivinay@gmail.com>
2023-02-26 14:22:24 +0530

    Update Find_Duplicate_in_array.cpp

--- Arrays/Find_Duplicate_in_array.cpp
+++ Arrays/Find_Duplicate_in_array.cpp
@@ -1,29 +1,27 @@
-// Given a read only array of n + 1 integers between 1 and n, find one number that repeats in linear time using less than O(n) space and traversing the stream sequentially O(1) times.
+Question:  https://leetcode.com/problems/find-the-duplicate-number/description/
 
-// Sample Input:
+Solution:
 
-// [3 4 1 4 1]
-// Sample Output:
-
-// 1
-// If there are multiple possible answers ( like in the sample case above ), output any one.
-
-// If there is no duplicate, output -1 .
-
-Solution: 
-  int Solution::repeatedNumber(const vector<int> &A) {
-    vector<int> b;
-    for(int i=0;i<A.size();i++)
-    {
-        b.push_back(A[i]);
-    }
-    sort(b.begin(),b.end());
-    for(int i=0;i<(b.size()-1);i++)
-    {
-        if(b[i] == b[i+1])
+class Solution {
+public:
+    //if there is duplicate in the array, it means there is cycle in the array
+    int findDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        int s = 0, f = 0;
+        s = nums[s];
+        f = nums[nums[f]];
+        while(s != f)
         {
-            return b[i];
+            s = nums[s];
+            f = nums[nums[f]];
         }
+        cout << s << " " << f << endl;
+        f = 0;
+        while(s != f)
+        {
+            s = nums[s];
+            f = nums[f];
+        }
+        return s;
     }
-    return -1;
-}
+};
