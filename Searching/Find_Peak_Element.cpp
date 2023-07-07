8c3069d647aa7137c43c7a5344611d1fa638a7c1
Pulavarti vinay kumar <pulavartivinay@gmail.com>
2023-07-07 23:50:37 +0530

    found peak element with O(log n) Time complexity using binary search

---
+++ Searching/Find_Peak_Element.cpp
@@ -0,0 +1,13 @@
+Question Link: https://leetcode.com/problems/find-peak-element/description/
+
+Solution:
+int findPeakElement(vector<int>& nums) {
+    int n = nums.size();
+    int start = 0, end = n-1;
+    while(start < end) {
+        int mid = start + ((end - start)/2);
+        if(nums[mid] < nums[mid+1]) start = mid+1;
+        else end = mid;
+    }
+    return start;
+}
