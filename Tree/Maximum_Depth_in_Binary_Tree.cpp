4f6b1ad5917b4c966b4d84f151005e2142e496c8
Pulavarti vinay kumar <pulavartivinay@gmail.com>
2023-07-22 13:48:23 +0530

    Solved using O(n) Auxiliary space complexity and O(n) time complexity

--- Tree/Maximum_Depth_in_Binary_Tree.cpp
+++ Tree/Maximum_Depth_in_Binary_Tree.cpp
@@ -1,7 +1,8 @@
 Question Link: https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
 
 Solution:
-int maxDepth(TreeNode* root) {
+int maxDepth(TreeNode* root) 
+{
     if(!root) return 0;
     else return 1 + max(maxDepth(root->left), maxDepth(root->right));
 }
