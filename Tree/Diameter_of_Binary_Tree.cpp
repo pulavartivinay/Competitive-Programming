61b5d7d6765a089b5d50331720c7a71ff81e0a34
Pulavarti vinay kumar <pulavartivinay@gmail.com>
2023-07-22 13:56:21 +0530

    Solved using O(n) a

--- Tree/Diameter_of_Binary_Tree.cpp
+++ Tree/Diameter_of_Binary_Tree.cpp
@@ -1,38 +1,20 @@
-Question Link: https://practice.geeksforgeeks.org/problems/diameter-of-binary-tree/1
+Question Link: https://leetcode.com/problems/diameter-of-binary-tree/description/
 
 Solution:
-
-int calculateMaxNode(Node* root)
+int getHeight(TreeNode* root, int& ans)
 {
     if(root)
     {
-        return 1 + max(calculateMaxNode(root->left), calculateMaxNode(root->right));
+        int a = getHeight(root->left, ans);
+        int b = getHeight(root->right, ans);
+        ans = max(ans, 1 + a + b);
+        return 1 + max(a,b);
     }
-    else
-    {
-        return 0;
-    }
+    return 0;
 }
-// Function to return the diameter of a Binary Tree.
-int diameter(Node* root) {
-    // Your code here
-    int a = 0;
-    int b = 0;
-    int c = 0;
-    int d = 0;
-    if(root)
-    {
-        if(root->left != NULL)
-        {
-            a = diameter(root->left);
-        }
-        if(root->right != NULL)
-        {
-            b = diameter(root->right);
-        }
-        c = calculateMaxNode(root->left);
-        d = calculateMaxNode(root->right);
-        return max(max(a,b),c+d+1);        
-    }
+int diameterOfBinaryTree(TreeNode* root) {
+    if(!root) return 0;
+    int ans = 1;
+    getHeight(root, ans);
+    return ans-1;
 }
-
