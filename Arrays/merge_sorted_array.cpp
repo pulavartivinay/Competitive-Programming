b7bf74fd8f82441e5b5eee8615289927e12a4857
Pulavarti vinay kumar <pulavartivinay@gmail.com>
2023-07-22 22:32:15 +0530

    Solved using O(1) space complexity and O(n+m) time complexity
    Take two pointers: one (i) is pointing to last element of first array (excluding zeros) and the other one (j) is pointing to last element to another array
    Third pointer pointing to last zero present in the first array
    will compare the values at i and j, then swap with third pointer until the second array pointer traverses the complete array

--- Arrays/merge_sorted_array.cpp
+++ Arrays/merge_sorted_array.cpp
@@ -1,24 +1,28 @@
 Question Link: https://leetcode.com/problems/merge-sorted-array/description/
 
 Solution:
-
-void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) 
-{
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
     if(n == 0) return;
-    int i = 0, j = 0;
-    while(i < m)
+    int i = m-1, j = n-1, pos = (n+m-1);
+    while(i >= 0 && j >= 0)
     {
-        if(nums1[i] > nums2[j]) 
+        if(nums1[i] <= nums2[j]) 
         {
-            swap(nums1[i], nums2[j]);
-            sort(nums2.begin(), nums2.end());
+            swap(nums2[j], nums1[pos]);
+            j--;
         }
-        i++;
+        else
+        {
+            swap(nums1[i], nums1[pos]);
+            i--;
+        }
+        pos--;
     }
-
-    for(int i=m;i<(m+n);i++)
+    while(j >= 0)
     {
-        nums1[i] = nums2[i-m];
+        swap(nums2[j], nums1[pos]);
+        j--;
+        pos--;
     }
     return;
 }
