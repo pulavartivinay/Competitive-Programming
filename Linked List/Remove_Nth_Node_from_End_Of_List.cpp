14aa1b395771c82bae0885d0de18a259349d8027
Pulavarti vinay kumar <pulavartivinay@gmail.com>
2023-07-23 14:04:15 +0530

    Solved using with just one traversal/pass of linked list

--- Linked List/Remove_Nth_Node_from_End_Of_List.cpp
+++ Linked List/Remove_Nth_Node_from_End_Of_List.cpp
@@ -1,39 +1,19 @@
 Question Link: https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
 
 Solution:
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
-public:
-    ListNode* removeNthFromEnd(ListNode* head, int n) {
-        int l = 0;
-        ListNode* tr = head;
-        while(tr)
-        {
-            l++;
-            tr = tr->next;
-        }
-        n = (l - n + 1);
-        if(n == 1) return head->next;
-        else
-        {
-            ListNode* prev = NULL;
-            ListNode* curr = head;
-            for(int i=1;i<n;i++)
-            {
-                prev = curr;
-                curr = curr->next;
-            }
-            prev->next = curr->next;
-        }
-        return head;
+ListNode* removeNthFromEnd(ListNode* head, int n) {
+    if(!head->next) return NULL; // base case n = 1
+    ListNode *first = head, *second = head;
+    for(int i=0;i<n;i++)
+    {
+        second = second->next;
     }
-};
+    if(!second) return head->next; // remove the first element of linked list
+    while(second->next != NULL)
+    {
+        first = first->next;
+        second = second->next;
+    }
+    first->next = first->next->next;
+    return head;
+}
