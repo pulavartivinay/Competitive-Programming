beb72138c0ff20fd629fe84eefe042b9f749c555
Pulavarti vinay kumar <pulavartivinay@gmail.com>
2024-01-26 21:28:07 +0530

    Solved using O(NlogN) time complexity and O(N) space complexity

--- Greedy/N_meetings_in_1_room.cpp
+++ Greedy/N_meetings_in_1_room.cpp
@@ -1,24 +1,35 @@
 Question: https://practice.geeksforgeeks.org/problems/n-meetings-in-one-room-1587115620/1
 
 Solution:
+static bool comp(pair<int,int> p1, pair<int,int> p2)
+{
+    if (p1.second < p2.second) return true;
+    else if (p1.second > p2.second) return false; 
+    else return (p1.first < p2.first);
+}
+//Function to find the maximum number of meetings that can
+//be performed in a meeting room.
 int maxMeetings(int start[], int end[], int n)
 {
     // Your code here
-    int ans = 1;
-    vector<pair<int,int>> store;
+    vector<pair<int,int>> intervals;
     for(int i=0;i<n;i++)
     {
-        store.push_back({end[i], start[i]});
+        intervals.push_back({start[i], end[i]});
     }
-    sort(store.begin(), store.end());
-    int end_time = store[0].first;
+    sort(intervals.begin(), intervals.end(), comp);
+    
+    int ans = 1;
+    int newEnd = intervals[0].second;
     for(int i=1;i<n;i++)
     {
-        if(store[i].second > end_time)
+        if(newEnd < intervals[i].first)
         {
+            newEnd = intervals[i].second;
             ans++;
-            end_time = store[i].first;
         }
+        else continue;
     }
+    
     return ans;
 }
