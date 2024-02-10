e76447529188069401008ab4c941902a537e6322
Pulavarti vinay kumar <pulavartivinay@gmail.com>
2024-02-10 23:05:34 +0530

    Solved using O(NlogN) time complexity and O(1) space complexity

--- Greedy/Minimum_Platforms.cpp
+++ Greedy/Minimum_Platforms.cpp
@@ -1,30 +1,18 @@
-Question: https://practice.geeksforgeeks.org/problems/minimum-platforms-1587115620/1#
+Question: https://www.geeksforgeeks.org/problems/minimum-platforms-1587115620/1
 
 Solution:
-
 int findPlatform(int arr[], int dep[], int n)
 {
   // Your code here
-
-  /*900----910
-                940----------------1200
-                   950-----1120
-                       1100----1130        1500--------1900
-                                                   1800------2000*/
-
-  int ans = 1;
   sort(arr, arr+n);
   sort(dep, dep+n);
-  int end_time = dep[0];
-  int curr_platform = 0;
-  for(int i=1;i<n;i++)
+  int ans = 1;
+  int i = 1, j = 0;
+  while(i < n && j < n)
   {
-      if(end_time < arr[i]) 
-      {
-          curr_platform++;
-          end_time = dep[curr_platform];
-      }
+      if(dep[j] < arr[i]) j++;
       else ans++;
+      i++;
   }
   return ans;
 }
