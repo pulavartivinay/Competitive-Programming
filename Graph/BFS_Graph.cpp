ae880af6e591dc997202963c9b290088d4b25b15
Pulavarti vinay kumar <pulavartivinay@gmail.com>
2024-02-24 15:58:45 +0530

    Update BFS_Graph.cpp

--- Graph/BFS_Graph.cpp
+++ Graph/BFS_Graph.cpp
@@ -1,28 +1,30 @@
 Question Link: https://practice.geeksforgeeks.org/problems/bfs-traversal-of-graph/1
 
 Solution:
-
-vector<int>bfsOfGraph(int V, vector<int> adj[])
-{
+vector<int> bfsOfGraph(int V, vector<int> adj[]) {
     // Code here
     vector<int> ans;
-    vector<bool> visited(V,0);
     queue<int> q;
+    vector<bool> visited(V, false);
     q.push(0);
-    visited[0] = 1;
+    ans.push_back(0);
+    visited[0] = true;
+    
     while(!q.empty())
     {
-        int li = q.front();
-        ans.push_back(li);
+        int curr_v = q.front();
         q.pop();
-        for(int i=0;i<adj[li].size();i++)
+        
+        for(int i=0;i<adj[curr_v].size();i++)
         {
-            if(!visited[adj[li][i]])
+            if(!visited[adj[curr_v][i]])
             {
-                q.push(adj[li][i]);
-                visited[adj[li][i]] = 1;
+                q.push(adj[curr_v][i]);
+                ans.push_back(adj[curr_v][i]);
+                visited[adj[curr_v][i]] = true;
             }
         }
     }
+    
     return ans;
 }
