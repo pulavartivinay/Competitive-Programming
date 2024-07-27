5e1ac2dcb1e3cc538acd1da605808cfe169b78c1
Pulavarti vinay kumar <pulavartivinay@gmail.com>
2024-07-27 14:44:16 +0530

    Solved using O(N) time complexity and O(1) space complexity

--- Linked List/Remove_Nth_Node_from_End_Of_List.cpp
+++ Linked List/Remove_Nth_Node_from_End_Of_List.cpp
@@ -1,19 +1,27 @@
 Question Link: https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
 
 Solution:
-ListNode* removeNthFromEnd(ListNode* head, int n) {
-    if(!head->next) return NULL; // base case n = 1
-    ListNode *first = head, *second = head;
-    for(int i=0;i<n;i++)
+ListNode* removeNthFromEnd(ListNode* head, int n)
+{
+    ListNode *slow = head, *fast = head;
+
+    int i=0;
+    while(i < n)
     {
-        second = second->next;
+        fast = fast->next;
+        i++;
     }
-    if(!second) return head->next; // remove the first element of linked list
-    while(second->next != NULL)
+    if(!fast) return head->next;
+
+    while(fast->next)
     {
-        first = first->next;
-        second = second->next;
+        slow = slow->next;
+        fast = fast->next;
     }
-    first->next = first->next->next;
+
+    ListNode* tmp = slow->next;
+    slow->next = tmp->next;
+    delete tmp;
+
     return head;
 }
