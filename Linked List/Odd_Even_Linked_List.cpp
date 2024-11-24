3722d2a8dfc3e1414f8d1a80f72bc9f0edc3d116
Pulavarti vinay kumar <pulavartivinay@gmail.com>
2024-11-24 12:48:26 +0530

    Solved using O(N) time complexity and O(1) space complexity

--- Linked List/Odd_Even_Linked_List.cpp
+++ Linked List/Odd_Even_Linked_List.cpp
@@ -1,21 +1,25 @@
-Question Link: https://leetcode.com/problems/odd-even-linked-list/description/?envType=list&envId=xler4hke
+Question Link: https://leetcode.com/problems/odd-even-linked-list/description/
 
 Solution:
 ListNode* oddEvenList(ListNode* head) {
-    if(!head || !head->next || !head->next) return head;
+    if(!head || !head->next || !head->next->next) return head;
+    ListNode *odd = head, *even = head->next, *prev = NULL, *merge_node = head->next;
 
-    ListNode *odd = head, *even = head->next;
-    ListNode *even_start = even;
-
-    while(odd->next && even->next)
+    while(odd && even)
     {
         odd->next = even->next;
+        if(!odd->next) prev = odd;
         odd = odd->next;
-        even->next = odd->next;
-        even = even->next;
+
+        if(odd)
+        {    
+            even->next = odd->next;
+            even = even->next;
+        }
     }
 
-    odd->next = even_start;
+    if(odd) odd->next = merge_node;
+    else prev->next = merge_node;
 
     return head;
 }
