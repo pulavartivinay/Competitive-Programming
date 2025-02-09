8c3abd1d6c2cf1774f42cd0f6680f69c9629d0a3
Pulavarti vinay kumar <pulavartivinay@gmail.com>
2025-02-09 19:28:14 +0530

    Solved using O(N) time complexity and O(

---
+++ Stack/Remove_stars_from_string.cpp
@@ -0,0 +1,26 @@
+Question Link: https://leetcode.com/problems/removing-stars-from-a-string/description/
+
+Solution:
+string removeStars(string s) {
+    int n = s.length();
+    stack<char> st;
+
+    for(int i=0;i<n;i++)
+    {
+        if(s[i] == '*')
+        {
+            if(!st.empty()) st.pop();
+        }
+        else st.push(s[i]);
+    }
+
+    string ans = "";
+    while(!st.empty())
+    {
+        ans += st.top();
+        st.pop();
+    }
+    reverse(ans.begin(), ans.end());
+
+    return ans;
+}
