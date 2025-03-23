5359a9f5e6c5bed56ea581f7253a9b3b5a7a16a0
Pulavarti vinay kumar <pulavartivinay@gmail.com>
2025-03-24 00:17:45 +0530

    Solved using O(N) time complexity and O(H) space complexity
    N --> number of nodes in the binary tree
    H --> maximum height of the binary tree

--- Tree/Lowest_Common_Ancestor.cpp
+++ Tree/Lowest_Common_Ancestor.cpp
@@ -1,16 +1,15 @@
-Question Link: https://www.geeksforgeeks.org/problems/lowest-common-ancestor-in-a-binary-tree/1
+Question Link: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/description/
 
 Solution:
-Node* lca(Node* root ,int n1 ,int n2 )
-{
-   //Your code here
-   Node *l = NULL, *r = NULL;
-   if(root->data == n1 || root->data == n2) return root;
-   if(root->left) l = lca(root->left, n1, n2);
-   if(root->right) r = lca(root->right, n1, n2);
-   
-   if(l && r) return root;
-   else if(l && !r) return l;
-   else if(!l && r) return r;
-   else return NULL;
-}
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+     if(!root) return NULL;
+
+     TreeNode *l = NULL, *r = NULL;
+     l = lowestCommonAncestor(root->left, p, q);
+     r = lowestCommonAncestor(root->right, p, q);
+     if(root == p || root == q) return root;
+     if(l && r) return root;
+     if(l) return l;
+     if(r) return r;
+     return NULL;
+ }
