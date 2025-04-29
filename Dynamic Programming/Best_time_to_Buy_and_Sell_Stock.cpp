767afc46b5fa0c55c00b44803b437310b274c700
Pulavarti vinay kumar <pulavartivinay@gmail.com>
2025-04-29 19:33:26 +0530

    Solved using O(N) time complexity and O(1) space complexity
    N --> length of the prices array

--- Dynamic Programming/Best_time_to_Buy_and_Sell_Stock.cpp
+++ Dynamic Programming/Best_time_to_Buy_and_Sell_Stock.cpp
@@ -1,16 +1,17 @@
-Question Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+Question Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
 
 Solution:
-
-int maxProfit(vector<int>& prices) 
-{
-    int minPrice = INT_MAX;
+int maxProfit(vector<int>& prices) {
+    int ans = 0;
     int n = prices.size();
-    int profit = 0;
+
+    int min_so_far = INT_MAX;
     for(int i=0;i<n;i++)
     {
-        if(prices[i] < minPrice) minPrice = prices[i];
-        else profit = max(profit,prices[i]-minPrice);
+        min_so_far = min(min_so_far, prices[i]);;
+
+        ans = max(ans, prices[i] - min_so_far);
     }
-    return profit;
+
+    return ans;
 }
