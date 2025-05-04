7ef898237dc180ca2746a78cbe204830e5d86fb3
Pulavarti vinay kumar <pulavartivinay@gmail.com>
2025-05-04 20:49:07 +0530

    Solved using O(N) time complexity and O(N) space complexity
    N --> size of the BST

--- Dynamic Programming/Unique_BST's.cpp
+++ Dynamic Programming/Unique_BST's.cpp
@@ -1,20 +1,23 @@
 Question Link: https://leetcode.com/problems/unique-binary-search-trees/description/
 
 Solution:
-int BST(vector<int>& dp, int n)
+int f(int n, vector<int>& dp)
 {
-    if(dp[n] != -1) return dp[n];
+    if(n <= 1) return 1;
 
+    if(dp[n] != -1) return dp[n];
     int ans = 0;
+
+    // root --> i
     for(int i=1;i<=n;i++)
     {
-        ans += BST(dp, i-1)*BST(dp, n-i);
+        ans += f(i-1, dp) * f(n-i, dp);
     }
-    return (dp[n] = ans);
+
+    return dp[n] = ans;
 }
 int numTrees(int n) {
-    vector<int> dp(n+1,-1);
-    dp[0] = 1;
-    dp[1] = 1;
-    return BST(dp, n);
+    vector<int> dp(n+1, -1);
+
+    return f(n, dp);
 }
