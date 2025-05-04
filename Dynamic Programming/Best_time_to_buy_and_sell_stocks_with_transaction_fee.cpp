7a87bbd981f27a05c5035562ed2ca80e8854d5db
Pulavarti vinay kumar <pulavartivinay@gmail.com>
2025-05-04 12:52:13 +0530

    Solved using O(N) time complexity and O(1) space complexity
    N --> length of the prices array

--- Dynamic Programming/Best_time_to_buy_and_sell_stocks_with_transaction_fee.cpp
+++ Dynamic Programming/Best_time_to_buy_and_sell_stocks_with_transaction_fee.cpp
@@ -1,58 +1,58 @@
 Question Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/description/
 
 Solution:
-int f(int ind, int flag, int n, vector<int>& prices, int fee, vector<vector<int>>& dp)
+int f(int i, int j, vector<int>& prices, int fee, vector<vector<int>>& dp)
 {
-    if(ind == n) return dp[ind][flag] = 0;
+    if(i == prices.size()) return 0;
 
-    if(dp[ind][flag] != -1) return dp[ind][flag];
+    if(dp[i][j] != -1) return dp[i][j];
 
-    int take = 0, notTake = 0;
-
-    if(flag)
+    // buy
+    if(j == 0)
     {
-        // buy
-        take = -prices[ind] + f(ind+1, !flag, n, prices, fee, dp);
-        notTake = f(ind+1, flag, n, prices, fee, dp);
-    }
-    else
-    {
-        // sell
-        take = ((prices[ind] - fee) + f(ind+1, !flag, n, prices, fee, dp));
-        notTake = f(ind+1, flag, n, prices, fee, dp);
+        int pick = (-prices[i]) + f(i+1, !j, prices, fee, dp);
+        int not_pick = f(i+1, j, prices, fee, dp);
+        return dp[i][j] = max(pick, not_pick);
     }
 
-    return dp[ind][flag] = max(take, notTake);
+    // sell
+    if(j == 1)
+    {
+        int pick = (prices[i] - fee) + f(i+1, !j, prices, fee, dp);
+        int not_pick = f(i+1, j, prices, fee, dp);
+        return dp[i][j] = max(pick, not_pick);
+    }
+
+    return 0;
 }
 int maxProfit(vector<int>& prices, int fee) {
     int n = prices.size();
     vector<int> prev(2, 0), curr(2, 0);
 
-    // base ase
-    prev[0] = prev[1] = 0;
-    
-    int take = 0, notTake = 0;
-    for(int ind=n-1;ind>=0;ind--)
+    // return f(0, 0, prices, fee, dp);
+
+    for(int i=n-1;i>=0;i--)
     {
-        for(int flag=0;flag<=1;flag++)
+        for(int j=1;j>=0;j--)
         {
-            if(flag)
+            // buy
+            if(j == 0)
             {
-                // buy
-                take = -prices[ind] + prev[!flag];
-                notTake = prev[flag];
-            }
-            else
-            {
-                // sell
-                take = (prices[ind] - fee) + prev[!flag];
-                notTake = prev[flag];
+                int pick = (-prices[i]) + prev[!j];
+                int not_pick = prev[j];
+                curr[j] = max(pick, not_pick);
             }
 
-            curr[flag] = max(take, notTake);
+            // sell
+            if(j == 1)
+            {
+                int pick = (prices[i] - fee) + prev[!j];
+                int not_pick = prev[j];
+                curr[j] = max(pick, not_pick);
+            }
         }
         prev = curr;
     }
 
-    return curr[1];
+    return prev[0];
 }
