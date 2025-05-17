bd7e25030540b3e9a263a278f169cdd69f52c547
Pulavarti vinay kumar <pulavartivinay@gmail.com>
2025-05-17 12:43:18 +0530

    Solved using O(V + E) time complexity and O(V) space complexity
    V --> number of vertices in the graph
    E --> number of edges in the graph

---
+++ Graph/Detect_Cycle_in_an_Undirected_Graph_BFS.cpp
@@ -0,0 +1,48 @@
+Question Link: https://www.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1
+
+Solution:
+bool detectCycle(int src, vector<vector<int>> adj, vector<int>& vis)
+{
+    queue<pair<int, int>> q;
+    vis[src] = true;
+    q.push({src, -1});
+
+    while(!q.empty())
+    {
+        pair<int, int> p = q.front();
+        q.pop();
+        int par = p.second;
+        int u = p.first;
+
+        for(int v:adj[u])
+        {
+            if(v != par)
+            {
+                if(vis[v]) return true;
+                vis[v] = true;
+                q.push({v, u});
+            }
+        }
+    }
+    
+    return false;
+}
+bool isCycle(int V, vector<vector<int>>& edges) {
+    // Code here
+    vector<int> vis(V, false);
+    vector<vector<int>> adj(V);
+    int u, v;
+    for(int i=0;i<edges.size();i++)
+    {
+        u = edges[i][0], v = edges[i][1];
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    
+    for(int i=0;i<V;i++)
+    {
+        if(!vis[i] && detectCycle(i, adj, vis)) return true;
+    }
+    
+    return false;
+}
